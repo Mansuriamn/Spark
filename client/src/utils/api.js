be46a470f561ca2ffc7d40c284a96615a8c95164
Pragati Mishra <144036589+Pragati-cloud@users.noreply.github.com>
2025-06-12 18:12:24 +0530

    detele axios file

--- client/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-// src/utils/api.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000", // ✅ Change to your backend server
-  withCredentials: true,
-});
-
-export default api;
